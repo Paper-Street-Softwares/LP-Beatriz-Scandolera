d4b6ae3ba160d02e6688257a2e4d40400a7d65d2
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-28 11:54:33 -0300

    fix: Finaliza revisões e insere blog

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -544,7 +544,7 @@ const content = {
       img: imgSteps,
       labelCards: "Ver mais",
       label: "Clique aqui para ver todas as matérias",
-      blogLink: "A_Definir", //sem "https://"
+      blogLink: "scandoleraadvocacia.wordpress.com", //sem "https://"
     },
     faq: {
       miniTag: "TIRE SUAS DÚVIDAS",
